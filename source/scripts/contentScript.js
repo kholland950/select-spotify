b7fb0d57ac85b20723bb8d8989ddf93e1da62fb3
kholland950 <kevinholland94@gmail.com>
2021-07-04 14:43:42 -0400

    Remove un-needed permissions from manifest

--- source/scripts/contentScript.js
+++ source/scripts/contentScript.js
@@ -1,12 +1,10 @@
-if (window.location.origin.includes('open.spotify.com')) {
-  const css =
-    '*[draggable] > [as="h1"] { user-select: all !important; }' +
-    '*[data-testid="tracklist-row"] [as="div"] { user-select: all !important; }';
+const css =
+  '*[draggable] > [as="h1"] { user-select: all !important; }' +
+  '*[data-testid="tracklist-row"] [as="div"] { user-select: all !important; }';
 
-  const head = document.head || document.getElementsByTagName('head')[0];
-  const style = document.createElement('style');
+const head = document.head || document.getElementsByTagName('head')[0];
+const style = document.createElement('style');
 
-  head.appendChild(style);
-  style.type = 'text/css';
-  style.appendChild(document.createTextNode(css));
-}
+head.appendChild(style);
+style.type = 'text/css';
+style.appendChild(document.createTextNode(css));
